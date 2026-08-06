17b73933ad4dddf457a0b83b904ad409c1b0529f
skyFonix <vlevente.main1123@gmail.com>
2026-08-06 22:20:38 +0200

    updated windowing system

--- src/Apps/Calculator/Calculator.jsx
+++ src/Apps/Calculator/Calculator.jsx
@@ -1,5 +1,79 @@
 import "./Calculator.css"
+import { useState } from "react"
 
 export function CalculatorApp() {
-    
+    const [equationArray, setEquationArray] = useState([])
+    const buttonArray = [
+        "7", "8", "9", "x",
+        "4", "5", "6", "/",
+        "1", "2", "3", "+",
+        "0", ".", "CA", "-"
+    ]
+
+    const handleClick = (e) => {
+        console.log(equationArray)
+        const { innerText } = e.target
+        const updatedArray = [...equationArray]
+        if (innerText == "CA") {
+            setEquationArray([])
+            return
+        }
+        if (/\d/.test(innerText) || innerText == ".") {
+            if (/\d/.test(equationArray[equationArray.length - 1])) {
+                updatedArray[equationArray.length - 1] = updatedArray[equationArray.length - 1] + innerText
+                setEquationArray(updatedArray)
+                return;
+            } else {
+                if (innerText == ".") {
+                    updatedArray.push("0" + innerText)
+                    setEquationArray(updatedArray)
+                    return
+                } else {
+                    updatedArray.push(innerText)
+                    setEquationArray(updatedArray)
+                    return
+                }
+            }
+        } else {
+            if (/\d/.test(equationArray[equationArray.length - 1])) {
+                console.log("a")
+                if (innerText == "x") {
+                    updatedArray.push("*")
+                } else {
+                    updatedArray.push(innerText)
+                }
+                setEquationArray(updatedArray)
+                return
+            } else {
+                if (innerText == "x") {
+                    updatedArray[equationArray.length - 1] = "*"
+                } else {
+                    updatedArray[equationArray.length - 1] = innerText
+                }
+                setEquationArray(updatedArray)
+                return
+            }
+        }
+    }
+
+    function handleCalculate() {
+        const result = String(eval(equationArray.join("")))
+        setEquationArray([result])
+    }
+
+
+    return (
+        <div className="app calculatorApp">
+            <div className="display">
+                <p className="equation">{equationArray}</p>
+                <p className="currentNumber">{equationArray[equationArray.length - 1]}</p>
+            </div>
+            <div className="keypad">
+                {buttonArray.map((btn) => (
+                    <button className={["x", "/", "+", "-"].includes(btn) ? "operator" : ""} onClick={handleClick}>{btn}</button>
+                ))}
+            </div>
+            <button onClick={handleCalculate}>=</button>
+        </div>
+    )
 }
--- src/Os.jsx
+++ src/Os.jsx
@@ -3,27 +3,35 @@ import { Window } from "./modules/Window/Window"
 import { AboutApp } from "./Apps/About/About"
 import { Notes } from "./Apps/Notes/Notes"
 import { SettingsApp } from "./Apps/Settings/Settings"
+import { CalculatorApp } from "./Apps/Calculator/Calculator"
 import { useEffect, useState } from "react"
 
 export default function Os() {
     const [windows, setWindows] = useState([])
 
-    const handleRemoveWindow = (i) => {
-        setWindows(items => items.filter((_, index) => index !== i));
-    }
+    const addWindow = (title) => {
+        setWindows(prev => [...prev, { id: crypto.randomUUID(), title }]);
+    };
 
-    function returnOpenedApp(key, title) {
-        switch (title) {
+    const handleRemoveWindow = (id) => {
+        setWindows(prev => prev.filter(win => win.id !== id));
+    };
+
+    function returnOpenedApp(key, window) {
+        switch (window.title) {
             case "About":
-                return (<Window title={title} onClose={(i) => { handleRemoveWindow(i) }} key={key} index={key} dimensions={{ x: 350, y: 400 }}><AboutApp /></Window>)
+                return (<Window title={window.title} onClose={(i) => { handleRemoveWindow(i) }} key={window.id} index={window.id} dimensions={{ x: 350, y: 400 }}><AboutApp /></Window>)
             case "Notes":
-                return (<Window title={title} onClose={(i) => { handleRemoveWindow(i) }} key={key} index={key} dimensions={{ x: 500, y: 400 }}><Notes /></Window>)
+                return (<Window title={window.title} onClose={(i) => { handleRemoveWindow(i) }} key={window.id} index={window.id} dimensions={{ x: 500, y: 400 }}><Notes /></Window>)
+            case "Calculator":
+                return (<Window title={window.title} onClose={(i) => { handleRemoveWindow(i) }} key={window.id} index={window.id} dimensions={{ x: 250, y: 320 }}><CalculatorApp /></Window>)
             case "Settings":
-                return (<Window title={title} onClose={(i) => { handleRemoveWindow(i) }} key={key} index={key} dimensions={{ x: 350, y: 150 }}><SettingsApp /></Window>)
+                return (<Window title={window.title} onClose={(i) => { handleRemoveWindow(i) }} key={window.id} index={window.id} dimensions={{ x: 350, y: 150 }}><SettingsApp /></Window>)
             default:
-                return (<Window title={title} onClose={(i) => { handleRemoveWindow(i) }} key={key} index={key} dimensions={{ x: 350, y: 500 }}></Window>)
+                return (<Window title={window.title} onClose={(i) => { handleRemoveWindow(i) }} key={window.id} index={window.id} dimensions={{ x: 350, y: 500 }}></Window>)
         }
     }
+
     function loadWallpaper() {
         try {
             let wallpaper = localStorage.getItem("wallpaperUrl")
@@ -47,19 +55,23 @@ export default function Os() {
         <main>
             <Bar></Bar>
             <div className="desktop">
-                <button onClick={() => { setWindows(prev => ([...prev, "About"])) }} className="openButton">
+                <button onClick={() => { addWindow("About") }} className="openButton">
                     <img src="/info.webp" alt="info icon" />
                     <p>Open About</p>
                 </button>
-                <button onClick={() => { setWindows(prev => ([...prev, "Notes"])) }} className="openButton">
+                <button onClick={() => { addWindow("Notes") }} className="openButton">
                     <img src="/notes.webp" alt="notes icon" />
                     <p>Open Notes</p>
                 </button>
-                <button onClick={() => { setWindows(prev => ([...prev, "Settings"])) }} className="openButton">
+                <button onClick={() => { addWindow("Calculator") }} className="openButton">
+                    <img src="/calculator.webp" alt="calculator icon" />
+                    <p>Open Calculator</p>
+                </button>
+                <button onClick={() => { addWindow("Settings") }} className="openButton">
                     <img src="/settings.webp" alt="settings icon" />
                     <p>Open Settings</p>
                 </button>
-                {windows.map((type, index) => returnOpenedApp(index, type))}
+                {windows.map((window, index) => returnOpenedApp(index, window))}
             </div>
         </main>
     )
